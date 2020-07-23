fc935f852f67c1b27d39f4b3115911b544f1dab6
ilionash <heebum1216@naver.com>
2020-07-23 15:43:56 +0000

    Create save_to_csv.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from indeed import get_jobs as get_indeed_jobs 
-from stackoverflow import get_jobs as get_stackoverflow_jobs 
+from stackoverflow import get_jobs as get_stackoverflow_jobs
+from save_to_csv import save_to_csv
 
+# Indeed Site 스크랩
 indeed_jobs = get_indeed_jobs() 
+
+# StackOverflow 스크랩
 stackoverflow_jobs = get_stackoverflow_jobs() 
 
+# Indeed + StackOverflow 스크랩 자료 합치기
 jobs = indeed_jobs + stackoverflow_jobs
 
-print(jobs)
+# csv로 저장하기
+save_to_csv(jobs)
---
+++ save_to_csv.py
@@ -0,0 +1,19 @@
+import csv
+
+def save_to_csv(jobs):
+  # 파일 열기 단, 파일이 없을 경우 생성해줌
+  # mode 3가지 설명
+  file = open("jobs.csv", mode="w")
+  
+  # csv를 작성할 수 있도록 하는 변수 생성
+  writer = csv.writer(file)
+
+  # row(행)의 값 추가
+  writer.writerow(["title", "company", "location", "link"])
+
+  for job in jobs:
+    # 딕셔너리에 저장된 값중 Value만 가지고 와서 row의 값 추가
+    # 단, dict.value()의 경우 type이 dict_value 이므로 list로 변환하여 저장해야 함
+    writer.writerow(list(job.values()))
+
+  return
